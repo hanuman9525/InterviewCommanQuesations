3a11759b551fcd3efb6b399e780dfe888e38992b
Hanuman Dinkar Adale <31722596+hanuman9525@users.noreply.github.com>
2018-03-21 10:04:37 +0530

    Update HalfDiamand.java

--- src/main/java/com/scp/congni/pattern/HalfDiamand.java
+++ src/main/java/com/scp/congni/pattern/HalfDiamand.java
@@ -4,16 +4,19 @@ public class HalfDiamand {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Diamond Shape");
 		HalfDiamand.pattern();
 	}
 
 	public static void pattern() {
+		//first half diamond
 		for (int i = 1; i < 5; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print(i);
 			}
 			System.out.println("");
 		}
+		//second half diamond
 		for (int i = 4; i >= 1; i--) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print(i);
