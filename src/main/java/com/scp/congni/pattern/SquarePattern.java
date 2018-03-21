0723bc432be7dddcda4a736c24f0bd524a9e6096
hanuman <hanumanadale@gmail.com>
2018-03-21 09:56:49 +0530

    Squre pattern feature

---
+++ src/main/java/com/scp/congni/pattern/SquarePattern.java
@@ -0,0 +1,19 @@
+package com.scp.congni.pattern;
+
+public class SquarePattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<4;j++)
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
